3bc4f9f9c685d7876c4cab3e1261feaf78fec41e
くつ <yuda0103@yahoo.co.jp>
2016-12-26 19:34:39 +0900

    エラー時にサムネイルを出力しないように修正、サムネイル出力を任意にできるようにラジオボタン選択式を追加

--- thumb_ga_pon.php
+++ thumb_ga_pon.php
@@ -3,7 +3,7 @@
 Plugin Name: サムネがぽんっ
 Description: ImageMagickで生成されたPDFのサムネイル画像を投稿に挿入することができるプラグイン
 Author: アクツ
-Version: 1.0
+Version: 1.0.1
 Author URI: http://ah-kutsu.net
 */
 
@@ -74,35 +74,36 @@ function thumbpon_err_msg_view() {
 // --------------------
 add_filter('attachment_fields_to_edit', 'thumbpon_add_attachment_field', 10, 2);
 function thumbpon_add_attachment_field( $form_fields, $post ) {
-	// if ( count(thumbpon_requirement_check()) > 0 ) { die(); } //エラーがあった場合は処理を終了
-	//ファイル形式がPDFだったらサムネ挿入フラグ用のフィールドを追加
-	if ( preg_match('/pdf/', $post->post_mime_type) ) {
-		$selected = get_post_meta( $post->ID, 'insert_thumb', true );
+	//エラーがなく、ファイル形式がPDFだったらサムネ挿入フラグ用のフィールドを追加
+	if ( count(thumbpon_requirement_check()) < 1 && preg_match('/pdf/', $post->post_mime_type) ) {
+		$selected    = get_post_meta( $post->ID, 'insert_thumb', true );
+		$checked_off = '';
+		$checked_on  = '';
 		if( !$selected || $selected == 'none' ) {
 			$checked_off = ' checked="checked"';
 		} else {
 			$checked_on = ' checked="checked"';
 		}
-		$html  = "<div class='thumbpon_insert_thumb'>";
-		$html .= "<input type='radio' name='attachments[$post->ID][insert_thumb]' id='insert_thumb-option-1' value='none'$checked_on />";
-		$html .= "<label for='insert_thumb-option-1'>挿入しない</label>";
-		$html .= "<input type='radio' name='attachments[$post->ID][insert_thumb]' id='insert_thumb-option-2' value='insert'$checked_off />";
-		$html .= "<label for='insert_thumb-option-2'>挿入する</label>";
-		$html .= "</div>";
+		$html  = '<div class="thumbpon_insert_thumb">';
+		$html .= '<input type="radio" name="attachments['.$post->ID.'][insert_thumb]" id="insert_thumb-option-1" value="none"'.$checked_off.' />';
+		$html .= '<label for="insert_thumb-option-1">挿入しない</label>';
+		$html .= '<input type="radio" name="attachments['.$post->ID.'][insert_thumb]" id="insert_thumb-option-2" value="insert"'.$checked_on.' />';
+		$html .= '<label for="insert_thumb-option-2">挿入する</label>';
+		$html .= '</div>';
 		$form_fields['insert_thumb'] = array(
 			'input' => 'html',
 			'html'  => $html,
 			'label' => 'サムネイルは',
 			'helps' => 'サムネイル付でリンクを挿入するか選択ください'
 		);
-		return $form_fields;
 	}
+	return $form_fields;
 }
 // フィールドの保存
 // --------------------
 add_filter('attachment_fields_to_save', 'thumbpon_save_field', 10, 2);
 function thumbpon_save_field($post, $attachment){
-	if ( isset($attachment['insert_thumb']) ) {
+	if ( count(thumbpon_requirement_check()) < 1 && isset($attachment['insert_thumb']) ) {
 		update_post_meta($post['ID'], 'insert_thumb', $attachment['insert_thumb']);
 	}
 	return $post;
@@ -113,12 +114,13 @@ function thumbpon_save_field($post, $attachment){
 // ---------------------------------------------------------------
 add_filter('media_send_to_editor','thumbpon_insert_thumb_to_editor', 10, 3);
 function thumbpon_insert_thumb_to_editor($html, $id, $attachment){
-	_log(get_post_meta($id));
-	if ( !preg_match('/pdf/', get_post($id)->post_mime_type) ) {
+	$insert     = get_post_meta($id)['insert_thumb'][0];
+	$thumb_path = wp_get_attachment_image($id, 'thumbnail');
+	//ファイル形式がPDFかつサムネを挿入を選択かつサムネのパスが取得出来た場合のみサムネイルを出力
+	if ( preg_match('/pdf/', get_post($id)->post_mime_type) && $insert === 'insert' && $thumb_path ) {
 		$thumb_path = wp_get_attachment_image($id, 'thumbnail');
-		$html       = '<a href="'.$attachment['url'].'" target="_blank" class="thumbpon_item">'.$thumb_path.$attachment['post_title'].'</a>';
+		$html       = '<a href="'.$attachment['url'].'" target="_blank" class="thumbpon_item"><span class="thumbpon_item__img">'.$thumb_path.'</span><span class="thumbpon_item__title">'.$attachment['post_title'].'</span></a>';
 	}
 	return $html;
 }
-
 ?>
